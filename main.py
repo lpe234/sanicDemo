03812129dfd74ab7c5ef9dbd70ac09707112bf80
lpe234 <lpe234@qq.com>
2017-07-15 10:11:39 +0800

    set config for dev, prod

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # -*- coding: UTF-8 -*-
+import socket
 
 from sanic import Sanic
 from sanic.response import json
+
+import settings
 
 from api_v1 import bp_v1
 from api_v2 import bp_v2
@@ -11,9 +14,20 @@ __author__ = 'lpe234'
 
 app = Sanic()
 
+# load config by hostname, and check config.
+host_name = socket.gethostname().capitalize().replace('.', '_').replace('-', '')
+app.config.from_object(getattr(settings, '{}sConfig'.format(host_name), settings.Config))
+if app.config.get('CONFIG_NAME'):
+    # FIXME: use log
+    print('load config from: {}'.format(app.config.get('CONFIG_NAME')))
+else:
+    # FIXME: use log
+    print('config may be wrong, please check with host_name: {}'.format(host_name))
+    exit()
+
 # register blueprints on the app
 app.blueprint(bp_v1, url_prefix='/v1')
-app.blueprint(bp_v2, url_prefix='/v1')
+app.blueprint(bp_v2, url_prefix='/v2')
 
 
 @app.get('/')
---
+++ settings.py
@@ -0,0 +1,47 @@
+# -*- coding: UTF-8 -*-
+
+__author__ = 'lpe234'
+
+
+"""
+config module
+"""
+
+
+class Config(object):
+    """
+    basic config
+    """
+    CONFIG_NAME = None
+    DEBUG = False
+    LOGO = """
+   _____             _      _____                       
+  / ____|           (_)    |  __ \                      
+ | (___   __ _ _ __  _  ___| |  | | ___ _ __ ___   ___  
+  \___ \ / _` | '_ \| |/ __| |  | |/ _ \ '_ ` _ \ / _ \ 
+  ____) | (_| | | | | | (__| |__| |  __/ | | | | | (_) |
+ |_____/ \__,_|_| |_|_|\___|_____/ \___|_| |_| |_|\___/     
+"""
+
+
+class ProductConfig(Config):
+    """
+    product config
+    """
+    pass
+
+
+class DevelopConfig(Config):
+    """
+    develop config
+    """
+    DEBUG = True
+
+
+class Lupengdemacbookpro_localConfig(DevelopConfig):
+    """
+    self config
+    """
+    CONFIG_NAME = 'LupengConfig'
+    pass
+
